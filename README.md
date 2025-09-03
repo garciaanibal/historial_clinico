844b2e80057867121ddb7c78a04921adda90098e
anibal <garciaanibal>
2025-09-03 00:15:59 -0300

    instalacion EA 4, Login, User, securityController, actualizacion archivo security

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-Comandos para correr el proyecto
-1- Clonar el repositorio
-2- Correr docker: docker compose up -d 
-3- Composer install 
-4- Crear el .env.local con los siguientes datos 
-DATABASE_URL=mysql://demo:demo@mariadb:3306/historialclinico
-5- Corre en http://localhost:17000/
-
 # 🏥 Historial Clínico - Symfony
 
 _Aplicación para gestión de historiales médicos._
@@ -20,7 +12,7 @@ _Aplicación para gestión de historiales médicos._
 git clone https://github.com/garciaanibal/historial_clinico.git
 cd historial_clinico
 docker compose up -d
-# Acceder al contenedor PHP (nombre puede variar según tu docker-compose.yml)
+# Acceder al contenedor PHP
 docker compose exec php-fpm bash
 # Dentro del contenedor:
 composer install
---
+++ migrations/Version20250903021714.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250903021714 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+          return 'Inserta usuario administrador inicial';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("INSERT INTO user (email, roles, password) 
+        VALUES ('adminhistorialclinico@gmail.com', '[\"ROLE_ADMIN\"]', '\$2y\$13\$zq1o4856VgXtYxOSUUPbH.e3mEIAKMYIZpYH4u/XORS72gxK2t4.W')");
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("DELETE FROM user WHERE email = 'adminhistorialclinico@gmail.com'");
+
+    }
+}
